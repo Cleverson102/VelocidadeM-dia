1847979d35337cae77682cbde48c24a6328da93c
Cleverson102 <169068234+Cleverson102@users.noreply.github.com>
2024-05-27 12:31:30 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa que calcula velocidade média em km/h");
+  System.out.println("");
+  System.out.println("A distância considerada é:");
+  double distancia = 400;
+  System.out.println(distancia);
+
+    System.out.println("O tempo gasto foi de:");
+    double tempo = 3;
+      System.out.println(tempo);
+
+    double velocidadeMedia = distancia / tempo;
+
+    
+    System.out.println("O resultado da Velocidade Média foi de:");
+    System.out.println(velocidadeMedia);
+  }
+
+}
